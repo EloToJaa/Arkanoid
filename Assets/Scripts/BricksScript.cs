5dd69c5217e2d6f4101b83a6cf005b502097c2d5
EloToJa <elotoja@protonmail.com>
2024-05-17 19:46:34 +0200

    Update BrickScript

--- Assets/Scripts/BricksScript.cs
+++ Assets/Scripts/BricksScript.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class BricksScript : MonoBehaviour
 {
-    public int life = 1;
+    public int lives = 1;
+
+    public void SetBrick(int life)
+    {
+        this.lives = life;
+        if(lives <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        BrickColor();
+    }
+
+    private void BrickColor()
+    {
+        var renderer = this.gameObject.GetComponent<Renderer>();
+        var colors = new Color[] { Color.red, Color.green, Color.blue, Color.yellow, Color.magenta, Color.cyan };
+        int colorIndex = Mathf.Clamp(lives - 1, 0, colors.Length - 1);
+        renderer.material.color = colors[colorIndex];
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Ball")
         {
-            life--;
+            lives--;
 
-            if (life <= 0)
+            if (lives <= 0)
             {
                 GameManager.instance.bricks.Remove(this.gameObject);
                 Destroy(this.gameObject);
