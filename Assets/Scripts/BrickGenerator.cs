922c3234ed0f7135d70e7df8d5efcf5316306bfe
EloToJa <elotoja@protonmail.com>
2024-05-17 19:58:47 +0200

    Create BrickGenerator

--- Assets/Scripts/BrickGenerator.cs
+++ Assets/Scripts/BrickGenerator.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BrickGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int[,] bricksArray = new int[,] {
+        {2,2,3,5,6,5,3,3, },
+        {2,2,3,4,5,4,3,2, },
+        {1,2,2,2,2,3,2,2, },
+        {0,2,2,2,2,3,2,0, },
+        {0,0,3,3,3,3,0,0, },
+        {0,0,0,3,3,0,0,0, },
+    };
+
+    public GameObject brick;
+
+    private void Start()
     {
-        
+        GenerateBricks(bricksArray);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenerateBricks(int[,] array)
     {
-        
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                var currentBrick = array[i, j];
+                if (currentBrick == 0) continue;
+
+                var position = new Vector3((-(array.GetLength(1) / 2) + (j * 1)), (4 - i * 0.3f), 0);
+
+                GameObject b = Instantiate(brick, position, Quaternion.identity);
+                b.transform.parent = gameObject.transform;
+                b.GetComponent<BricksScript>().SetBrick(currentBrick);
+            }
+        }
+        GameManager.instance.bricks.AddRange(GameObject.FindGameObjectsWithTag("Brick"));
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,8 +18,6 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
-
-        bricks.AddRange(GameObject.FindGameObjectsWithTag("Brick"));
     }
 
     private void Update()
